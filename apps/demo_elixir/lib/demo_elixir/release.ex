40fbfb3f94bc79e157cabe776c8a378c3c20f265
vince roy <vince@potionapps.com>
2022-03-22 15:39:08 -0400

    use the provided default release file

--- apps/demo_elixir/lib/demo_elixir/release.ex
+++ apps/demo_elixir/lib/demo_elixir/release.ex
@@ -5,10 +5,21 @@ defmodule DemoElixir.Release do
   """
   @app :demo_elixir
 
+  defp ensure_repo_created(repo) do
+    IO.puts("create #{inspect(repo)} database if it doesn't exist")
+
+    case repo.__adapter__.storage_up(repo.config) do
+      :ok -> :ok
+      {:error, :already_up} -> :ok
+      {:error, term} -> {:error, term}
+    end
+  end
+
   def migrate do
     load_app()
 
     for repo <- repos() do
+      :ok = ensure_repo_created(repo)
       {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
     end
   end
